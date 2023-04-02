36218b5d1907102e17a6aa65eea5064f15a1dc07
SyBLO <39127586+SyBLO@users.noreply.github.com>
2023-04-02 22:27:07 +0200

    update

--- Server/Data/ApplicationDbContext.cs
+++ Server/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using ServiceDT.Server.Models;
@@ -7,24 +9,49 @@ using ServiceDT.Shared.Models;
 
 namespace ServiceDT.Server.Data
 {
-    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
-    {             
+    //public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+   // {
+   //     public DbSet<SuiviBE> SuiviBEs { get; set; }
+   //     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,
+    //    IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+    //    {
+    //    }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,
-        IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
-        {
-        }
 
-        public DbSet<SuiviBE> SuiviBEs { get; set; }
-        public DbSet<ActionItem> ActionItems { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-           modelBuilder.Entity<ActionItem>()
-                  //.HasNoKey()
-                  .HasOne(d => d.SuiviBE)
-                  .WithMany(d => d.ActionItems);
-        }
-
+        // public DbSet<ActionItem> ActionItems { get; set; }
+        //  protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //   modelBuilder.Entity<ActionItem>()
+        //           //.HasNoKey()
+        //           .HasOne(d => d.SuiviBE)
+        //           .WithMany(d => d.ActionItems);
+        //   }
+        //
 
     }
-}
+
+
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+        //public ApplicationDbContext(DbContextOptions options) : base(options)
+       // {
+       // }
+       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,
+        IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+       {
+       }
+        public DbSet<SuiviBE> SuiviBEs { get; set; }
+        public DbSet<ActionItem> ActionItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ActionItem>()
+                .HasOne(p => p.SuiviBE)
+                .WithMany(b => b.ActionItems);
+        }
+
+       
+ }
+
