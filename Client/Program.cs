31f70daa2acf0dd23147502d9c4a8d6e5d346f2c
SyBLO <39127586+SyBLO@users.noreply.github.com>
2023-04-02 22:26:12 +0200

    updated

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,4 @@
+using Append.Blazor.Sidepanel;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,6 +9,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSidepanel();
+
 builder.Services.AddHttpClient("ServiceDT.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
@@ -16,7 +19,7 @@ builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().Cre
 
 
 builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
- 
+
 
 builder.Services.AddApiAuthorization();
 
